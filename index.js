750787c1f8addbaa857636f1c75205f11abf54c4
Francisca <francisca.orwa@student.moringaschool.com>
2022-11-21 14:59:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+const currentUser = 'name';
+currentUser;
+
+const welcomeMessage =`Welcome to Flatbook, ${currentUser}!`;
+ welcomeMessage;
+
+const excitedWelcomeMessage=welcomeMessage.toUpperCase();
+welcomeMessage;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+shortGreeting;
+
+
+
+
+
+
+    
+    
